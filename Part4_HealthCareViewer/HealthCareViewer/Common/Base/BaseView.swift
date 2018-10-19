81fb4d6b9b4fa68cfc3045c4829f9d045c82841a
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2018-10-19 23:06:40 +0700

    Starter part 4

---
+++ Part4_HealthCareViewer/HealthCareViewer/Common/Base/BaseView.swift
@@ -0,0 +1,24 @@
+//
+//  BaseView.swift
+//  HealthCareViewer
+//
+//  Created by MACOS on 10/19/18.
+//  Copyright © 2018 MACOS. All rights reserved.
+//
+
+import UIKit
+
+public class BaseUIView: UIView, ReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func initialize() {}
+    
+}
---
+++ Part4_HealthCareViewer/HealthCareViewer/Common/Style/Resource.swift
@@ -0,0 +1,38 @@
+//
+//  Resource.swift
+//  HealthCareViewer
+//
+//  Created by MACOS on 10/19/18.
+//  Copyright © 2018 MACOS. All rights reserved.
+//
+
+import UIKit
+
+public class Resource {
+    
+    public class Images {
+        public static var blueLogoImage: UIImage? = UIImage(named: "LogoBlue")
+    }
+    
+    public class Title {
+        
+        public class SignInAndSignUp {
+            public static var patientCodeTitle = "Mã số bệnh nhân / Bác sĩ"
+            public static var patientCodePlaceHolder = "Nhập mã số"
+            public static var passwordTitle = "Mật khẩu"
+            public static var passwordPlaceholder = "Nhập mật khẩu"
+            public static var signIn = "Đăng nhập"
+            public static var signUp = "Đăng kí"
+        }
+        
+        public class Home {
+            
+        }
+        
+    }
+    
+}
+
+
+
+
